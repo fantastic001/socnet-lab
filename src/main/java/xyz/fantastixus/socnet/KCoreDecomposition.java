64df9f3579fbb81a9c775b446ce97cf73db80489
Stefan Nožinić <stefan@lugons.org>
2022-07-03 02:13:01 +0200

    K-core decomposition initial definitions

---
+++ src/main/java/xyz/fantastixus/socnet/KCoreDecomposition.java
@@ -0,0 +1,24 @@
+package xyz.fantastixus.socnet;
+
+import java.util.List;
+
+import edu.uci.ics.jung.graph.UndirectedGraph;
+
+public class KCoreDecomposition<V, E> {
+    public KCoreDecomposition(UndirectedGraph<V, E> graph) {
+
+    }
+
+    public void decompose() {
+
+    }
+
+    public int getShellIndex(V v) {
+        return 0;
+    }
+
+    public List<UndirectedGraph<V, E>> getSubnetworks() {
+        return null;
+    }
+    
+}
